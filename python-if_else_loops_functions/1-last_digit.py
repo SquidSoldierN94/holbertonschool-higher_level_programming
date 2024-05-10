fa496c8af87d8cccc4216854b5ea3de2733f8cd8
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-05-10 09:54:52 +0200

    Stupeflip - A a bas la hiérarchie

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,14 @@
 import random
 
 number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
 
-x = abs(number) % 10
-
-if x == 0:
-    print(f"Last digit of {number} is {x} and is 0")
-elif x > 5:
-    print(f"Last digit of {number} is {x} and is greater than 5")
-elif x < 6 and not 0:
-    print(f"Last digit of {number} is {x} and is less than 6 and not 0")
-elif x < 0:
-    print(f"Last digit of {number} is -{x} and is less than 6 and not 0")
+if last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
+elif last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+else:
+    if number < 0:
+        print(f"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
+    else:
+        print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
