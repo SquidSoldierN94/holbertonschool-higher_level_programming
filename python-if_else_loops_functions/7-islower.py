86197c3ef107166f35dcbd0d2f99d420c5d9f262
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-05-11 13:34:16 +0200

    we jamin'

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for x in range(128):
-        if x == ord(c):
-            if 97 <= x < 123:  # Check if the character is lowercase
-                print(f"{c} => lower")
-            elif 65 <= x < 91:  # Check if the character is uppercase
-                print(f"{c} => upper")
-            break
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
