6cc313ea81c6616bd85cf91ca65ece57b2e5a84c
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-05-11 13:52:49 +0200

    maggots swimming in their own vomit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def uppercase(str):
+    lower = ''
+    for char in str:
+        if 'a' <= char <= 'z':
+            str = chr(ord(char) - 32)
+        else:
+            lower = char
+        str += lower
+    print(f"{lower}")
