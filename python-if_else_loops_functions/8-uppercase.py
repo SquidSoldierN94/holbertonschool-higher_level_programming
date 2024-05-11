2270d0b00aee94a84e1249c6d02d54cd630b837b
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-05-11 13:57:13 +0200

    pepsi + milk = pilk

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    lower = ''
-    for char in str:
-        if 'a' <= char <= 'z':
-            str = chr(ord(char) - 32)
-        else:
-            lower = char
-        str += lower
-    print(f"{lower}")
+    new_str = ""
+    for i in range(len(str)):
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
+            new_str += chr(ord(str[i]) - 32)
+            continue
+        new_str += str[i]
+
+    print('{0}'.format(new_str))
