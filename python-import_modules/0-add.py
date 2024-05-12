659e1f44a852fd67d9e8e3b51c8d6dcd9e180e97
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-05-12 20:14:57 +0200

    ITS SO FUNNY

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-
-from add_0 import add
-
 if __name__ == "__main__":
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    from add_0 import add
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
