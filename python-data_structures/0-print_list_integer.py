480a40766e2ff580133c3d0e4bf66c704d0dd056
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-05-13 11:25:04 +0200

    GOD DAMMIT

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    formated_string = my_list.format([])
-    print(f"{formated_string}")
+    for num in my_list:
+        print("{:d}".format(num))
