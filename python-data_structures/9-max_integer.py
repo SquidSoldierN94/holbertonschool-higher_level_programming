f0eae669fcf110d248c2645fcb583030f6f42919
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-05-13 18:00:14 +0200

    Carpenter Brut - Trilogy

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    
     largest_number = my_list[0]
     for num in my_list:
         if num > largest_number:
             largest_number = num
-    
     return largest_number
