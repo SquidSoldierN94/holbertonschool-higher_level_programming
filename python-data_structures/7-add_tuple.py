9e8462a370fcac507d70400dcc3a1a162725547c
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-05-13 17:12:25 +0200

    out like a like

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    element_a = tuple_a[1] if len(tuple_a) >= 2 else 0
-    element_b = tuple_b[1] if len(tuple_b) >= 2 else 0
-
-    result_second_element = element_a + element_b
-
-    return (tuple_a[0] + tuple_b[0], result_second_element)
+    first_element_a = tuple_a[0] if len(tuple_a) >= 1 else 0
+    first_element_b = tuple_b[0] if len(tuple_b) >= 1 else 0
+    second_element_a = tuple_a[1] if len(tuple_a) >= 2 else 0
+    second_element_b = tuple_b[1] if len(tuple_b) >= 2 else 0
+    result_first_element = first_element_a + first_element_b
+    result_second_element = second_element_a + second_element_b
+    return (result_first_element, result_second_element)
