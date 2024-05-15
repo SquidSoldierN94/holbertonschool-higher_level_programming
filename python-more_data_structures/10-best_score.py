db43c57237b517d5b1367b8b3da38320951c4512
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-05-15 10:15:59 +0200

    Orden Ogan - One Last Chance

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,12 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    
     best_key = None
     highest_score = float('-inf')
     for key, value in a_dictionary.items():
         if value > highest_score:
             highest_score = value
             best_key = key
-    
     return best_key
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,10 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
-    
     roman_numerals = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
-
     result = 0
     prev_value = 0
     for symbol in reversed(roman_string):
@@ -17,5 +15,4 @@ def roman_to_int(roman_string):
         else:
             result += value
         prev_value = value
-
     return result
