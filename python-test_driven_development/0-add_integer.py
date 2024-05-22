90dd10dcd89dccd4f08e130cdedb2f0713bf3398
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-05-22 13:51:32 +0200

    Dance with the Dead - Driven to Madness [Full Album]

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module provides a function to add two integers or floats.
+"""
+
 def add_integer(a, b=98):
     """Adds two integers or floats, casting them to integers first."""
     if not isinstance(a, (int, float)):
