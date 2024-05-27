4e92892b6dd9496d917713db031de1abf795e742
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-05-27 13:36:31 +0200

    Ave Maria

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -21,4 +21,8 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    return "My name is " + first_name + " " + last_name
+
+    if last_name:
+        return "My name is {} {}".format(first_name, last_name)
+    else:
+        return "My name is {}".format(first_name)
