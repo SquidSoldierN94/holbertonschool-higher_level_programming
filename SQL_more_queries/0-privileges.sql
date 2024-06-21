abfd681e8d13b29966355b03266f13252f919360
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-06-21 21:20:16 +0200

    Hotline Miami 2: Wrong Number Soundtrack - Pursuit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,19 +1,3 @@
--- Script to manage MySQL users and their privileges.
-
--- Create user 'user_0d_1' if it does not already exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
--- Create user 'user_0d_2' if it does not already exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Show grants for 'user_0d_1'
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Show grants for 'user_0d_2'
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
--- Revoke all privileges for user_0d_1 if they exist
-REVOKE ALL PRIVILEGES ON *.* FROM 'user_0d_1'@'localhost';
-
--- Grant USAGE privilege only
-GRANT USAGE ON *.* TO 'user_0d_1'@'localhost';
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
