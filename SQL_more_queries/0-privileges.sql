9d66ef39ddbe33d626cb3654cf0597d0f18689fa
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-06-21 21:12:24 +0200

    Hotline Miami 2: Wrong Number Soundtrack - Bad Ending

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,13 @@
+-- Script to manage MySQL users and their privileges.
+
+-- Create user 'user_0d_1' if it does not already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+
+-- Create user 'user_0d_2' if it does not already exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Show grants for 'user_0d_1'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show grants for 'user_0d_2'
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
