308a1a92ee3ed5fc27cd0c158de42004ba1329c0
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-06-21 21:03:03 +0200

    Hotline Miami 2: Wrong Number Soundtrack - Bad Ending

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
