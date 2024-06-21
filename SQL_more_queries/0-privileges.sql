aa6fece5e8f2c5635eb4a8d59c44bca4d2a6c4f0
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-06-21 21:15:36 +0200

    Hotline Miami 2: Wrong Number Soundtrack - Pursuit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -5,9 +5,3 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 
 -- Create user 'user_0d_2' if it does not already exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Show grants for 'user_0d_1'
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Show grants for 'user_0d_2'
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
