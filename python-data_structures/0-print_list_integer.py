ed2e72d4b5140a9a70e428334b8e82295792a427
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-10-21 07:58:04 +0200

    fan2stup

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=None):
-    if my_list is None:
-        my_list = []
-    for number in my_list:
-        print("{}".format(number))
+def print_list_integer(my_list=[]):
+
+    for index in range(len(my_list)):
+        print("{:d}".format(my_list[index]))
--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for number in my_list[::-1]:
-        print("{:d}".format(number))
+    my_list.reverse()
+
+    for index in range(len(my_list)):
+        print("{:d}".format(my_list[index]))
