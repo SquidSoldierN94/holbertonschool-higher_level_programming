44b09b90694f405fc96680fdc28e7792acad9d24
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-10-21 08:26:08 +0200

    le spleen des petits a l'ecole sa les rend marteau

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """
-This module defines a function called lookup
+Module retrieve a list from inherited list and sort
+it in ascending order
 """
 
 
-def lookup(obj):
+class MyList(list):
     """
-    Function to return a list of available attributes
-    and methods of an object
+    Class that inherits from "list"
     """
 
-    attribute_list = dir(obj)
-    return attribute_list
+    def print_sorted(self):
+        """
+        Function that prints the list but in ascending order
+        """
+
+        print(sorted(self))
