ed656b3670df8718bcc19edaecf6ff98c9b4dec1
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-10-21 07:42:55 +0200

    sandrine cacheton

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-
-    new_matrix = []
-
-    for row in matrix:
-        new_row = [num ** 2 for num in row]
-
-        new_matrix.append(new_row)
-
-    return new_matrix
+    return [[num ** 2 for num in row] for row in matrix]
