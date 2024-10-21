f03984f9615e1818d5d6a83cfb1eb2360337689d
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-10-21 07:49:31 +0200

    aurevoir stupeflip

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[num ** 2 for num in row] for row in matrix]
+
+    new_matrix = []                         # Create a new empty matrix
+
+    for row in matrix:                      # Iterate through each lines
+        # Create a new line with square of each
+        new_row = [num ** 2 for num in row]
+
+        new_matrix.append(new_row)          # Append new row to the new matrix
+
+    return new_matrix
