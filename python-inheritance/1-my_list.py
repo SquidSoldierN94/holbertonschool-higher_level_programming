8e72efc23a9b272479d490ff5ab80c56bc736488
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-10-21 08:31:01 +0200

    sado modo est reglo

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
+"""
+Module retrieve a list from inherited list and sort
+it in ascending order
+"""
+
 
 class MyList(list):
     """
-    MyList is a subclass of the built-in list class.
-    
-    This class inherits from the built-in list and provides 
-    an additional method to print the list in a sorted order.
+    Class that inherits from "list"
     """
 
     def print_sorted(self):
         """
-        Prints the list in ascending order.
-        
-        The method uses the built-in sorted() function to create 
-        a new sorted list from the original list and prints it.
-        The original list remains unchanged.
+        Function that prints the list but in ascending order
         """
-        print(sorted(self))  # Print the sorted version of the list
+
+        print(sorted(self))
