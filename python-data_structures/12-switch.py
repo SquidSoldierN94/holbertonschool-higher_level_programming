10ab47f071cde20223f2b890c8e721ffea37676b
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-11-26 10:30:13 +0100

    triggers my OCD

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,5 +1,35 @@
 #!/usr/bin/python3
-a = 89
-b = 10
-a, b = b, a
-print("a={:d} - b={:d}".format(a, b))
+def roman_to_int(roman_string):
+    """
+    Convert a Roman numeral to an integer.
+
+    Parameters:
+    roman_string (str): The Roman numeral string to convert.
+
+    Returns:
+    int: The integer representation of the Roman numeral. Returns 0 if the input is not a string or is None.
+    """
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = roman_numerals.get(char, 0)
+        
+        if value == 0:
+            return 0
+        
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        
+        prev_value = value
+
+    return total
--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python3
+
 """
-This module provides a function to add two integers.
+This module provides a function for basic add operation.
+Adds two numbers together (with "b" = 98 by default)
+Return: sum
 """
+
 
 def add_integer(a, b=98):
     """
-    Adds two integers or floats, casting them to integers if necessary.
-
-    Args:
-        a (int/float): The first number.
-        b (int/float): The second number, default is 98.
-
-    Raises:
-        TypeError: If a or b are not integers or floats.
-
-    Returns:
-        int: The sum of a and b.
+    This function adds two numbers together and return the result.
     """
+
+# Error if not int or float
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
-    return int(a) + int(b)
+
+# Error if infinite or -infinite
+
+    if a == float('inf') or a == float('-inf'):
+        raise OverflowError("cannot convert float infinity to integer")
+    if b == float('inf') or b == float('-inf'):
+        raise OverflowError("cannot convert float infinity to integer")
+
+
+# Error if "NaN"
+
+    if a != a:
+        raise ValueError("cannot convert float NaN to integer")
+    if b != b:
+        raise ValueError("cannot convert float NaN to integer")
+
+# If everything correct, cast in int
+
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
