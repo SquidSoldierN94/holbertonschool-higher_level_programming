c6e5582790e2d3805e935b6a05bd5c1483cad25d
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-12-06 09:45:14 +0100

    the siege of Ruby Ridge

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,27 +1,3 @@
 #!/usr/bin/python3
-
-"""
-This module defines a function `copy_list` that creates a copy of a given list.
-
-The opy of the input list, meaning that the original
-list remains unchanged, and any modifications to the new list will not affect
-the original one.
-
-The function can handle lists containing any type of objects.
-
-No external modules are required for this implementation.
-
-Function:
-    - copy_list(a_list: List[Any]) -> List[Any]:
-        Returns a new list containing the same elements as the input list.
-
-Parameters:
-    a_list (List[Any]): The input list to be copied.
-
-Returns:
-    List[Any]: A new list containing the same elements as the input list.
-"""
-
-
 def copy_list(a_list):
     return a_list[:]
