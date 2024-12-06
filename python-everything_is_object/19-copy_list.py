61b8b627c8ae2de7eb2ae1bd6f6854298827bd9a
SquidSoldierN94 <esteban.guillen.pro@gmail.com>
2024-12-06 08:34:28 +0100

    ugh

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,18 +1,4 @@
 #!/usr/bin/python3
 
 def copy_list(a_list):
-    """
-    Creates a shallow copy of the provided list.
-
-    This function takes a list as input and returns a new list that contains the
-    same elements. The new list is a shallow copy, meaning that if the original
-    list contains mutable objects (e.g., lists, dictionaries), those objects
-    are not copied but merely referenced.
-
-    Parameters:
-        a_list (list): The list to be copied.
-
-    Returns:
-        list: A new list containing the same elements as the input list.
-    """
-    return [item for item in a_list]
+    return list(a_list)
